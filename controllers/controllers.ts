84260384dfcf0904b7fb9285000fc3d56cb4bab8
yunchao <tanyunchao@gmail.com>
2023-06-25 19:48:49 +0800

    Updated schema and request for rooms

--- controllers/controllers.ts
+++ controllers/controllers.ts
@@ -4,10 +4,10 @@ import { google } from 'googleapis';
 
 // create a new meeting code 
 export const createMeeting =  async (req: Request, res: Response) => {
-    const {code, owner} = req.body
+    const {roomId, roomPassword, emails} = req.body
 
     try {
-        const meeting = await MeetingCode.create({code, owner})
+        const meeting = await MeetingCode.create({roomId, roomPassword, emails})
         res.status(200).send(meeting)
     } catch (error: any) { // will change later
         res.status(400).json({error: error.message})
@@ -26,8 +26,8 @@ export const getEvent = async (req: Request, res: Response) => {
 
   const requestBody = {
     "items": items,
-    "timeMin": "2023-05-01T00:00:00+08:00",
-    "timeMax": "2023-05-31T23:59:59+08:00",
+    "timeMin": "2023-06-25T00:00:00+08:00",
+    "timeMax": "2023-07-01T00:00:00+08:00",
     "timeZone": "Asia/Singapore"
   }
 
--- models/MeetingCode.model.ts
+++ models/MeetingCode.model.ts
@@ -1,13 +1,15 @@
 import { model, Schema, Model, Document } from 'mongoose';
 
 export interface IMeetCode extends Document {
-    code: string;
-    owner: string;
+    roomId: string;
+    roomPassword: string;
+    emails: string;
 }
 
 const MeetingCodeSchema: Schema = new Schema({
     code: {type: String, required: true},
     owner: {type: String, required: true},
+    emails: {type: String, required: true},
 }, {timestamps: true})
 
 export const MeetingCodeModel: Model<IMeetCode> = model<IMeetCode>('MeetingCode', MeetingCodeSchema)
