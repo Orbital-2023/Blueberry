8fc320ec2a67358ca88e1f4ae045d2974b36b83a
yunchao <tanyunchao@gmail.com>
2023-06-25 16:43:35 +0800

    Allow mapping of emails in API call body

--- controllers/controllers.ts
+++ controllers/controllers.ts
@@ -16,24 +16,26 @@ export const createMeeting =  async (req: Request, res: Response) => {
 }
 
 export const getEvent = async (req: Request, res: Response) => {
-    const requestBody = {
-      "calendarExpansionMax": 10,
-      "groupExpansionMax": 10,
-      "items": [
-        {
-          "id": process.env.DUMMY_EMAIL
-        }
-      ],
-      "timeMin": "2023-05-01T00:00:00+08:00",
-      "timeMax": "2023-05-31T23:59:59+08:00",
-      "timeZone": "Asia/Singapore"
-    }
+  const emails = process.env.TEST_EMAILS!.split(", ");
 
-    try {
-        const calendar = google.calendar({ version: "v3", auth: process.env.GOOGLE_API_KEY });
-        const response = await calendar.freebusy.query({requestBody});
-        res.status(200).json(response)
-    } catch (e: any) {
-        res.status(400).json({e: e.mesage})
-    }
+  const items = emails.map((email) => {
+      return {
+        id: email,
+      };
+  });
+
+  const requestBody = {
+    "items": items,
+    "timeMin": "2023-05-01T00:00:00+08:00",
+    "timeMax": "2023-05-31T23:59:59+08:00",
+    "timeZone": "Asia/Singapore"
   }
+
+  try {
+      const calendar = google.calendar({ version: "v3", auth: process.env.GOOGLE_API_KEY });
+      const response = await calendar.freebusy.query({requestBody});
+      res.status(200).json(response)
+  } catch (e: any) {
+      res.status(400).json({e: e.mesage})
+  }
+}
