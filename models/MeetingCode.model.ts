b69976f355b38b687a182c6b79d0e1fd7befa91c
yunchao <tanyunchao@gmail.com>
2023-07-16 22:35:12 +0800

    Updated schema to include date-time

--- models/MeetingCode.model.ts
+++ models/MeetingCode.model.ts
@@ -4,12 +4,16 @@ export interface IMeetCode extends Document {
     roomId: string;
     roomPassword: string;
     emails: string;
+    startDate: string;
+    endDate: string;
 }
 
 const MeetingCodeSchema: Schema = new Schema({
     roomId: {type: String, required: true},
     roomPassword: {type: String, required: true},
     emails: {type: String, required: true},
+    startDate: {type: String},
+    endDate: {type: String},
 }, {timestamps: true})
 
 export const MeetingCodeModel: Model<IMeetCode> = model<IMeetCode>('MeetingCode', MeetingCodeSchema)
